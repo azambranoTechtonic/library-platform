eb07946126b8048ad7f1b7cfec3190cf2e44dbb0
Alexis Zambrano <alexis.zambrano@techtonic.com>
2018-10-02 15:01:13 -0600

    Init commit

---
+++ js/book.js
@@ -0,0 +1,12 @@
+
+function Book(title, author, numPages, pubData) {
+  this.title = new title;
+  this.author = new author;
+  this.title = new title;
+  this.title = new title;
+};
+
+Book.prototype.editBook = function(title)
+{
+
+};
---
+++ js/engine.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(e){
+    window.gLibrary = new Library();
+});
+
+function Library() {
+  this.bookShelf = new Array();
+};
+
+
+Library.prototype.addBook = function(book) {
+
+};
+
+Library.prototype.removeBookByTiltle = function() {
+
+};
