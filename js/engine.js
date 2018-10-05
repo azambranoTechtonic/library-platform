5a5eeb9384682c4945795172137f51db955347c7
Alexis Zambrano <alexis.zambrano@techtonic.com>
2018-10-05 14:06:37 -0600

    Variable names update, commented getBookByAuthor

--- js/engine.js
+++ js/engine.js
@@ -13,47 +13,47 @@
 Library.prototype.addBook = function(book)
 {
   // Add a book to bookShelf
-  var bAdd = true;
+  var isAdded = true;
 
   if (this.checkBookByTitle(book.title).length < 1) {
     this.bookShelf.push(book);
   } else {
-    bAdd = false;
+    isAdded = false;
   }
 
-  return bAdd; //True if not already added - false if it already added
+  return isAdded; //True if not already added - false if it already added
 };
 
 Library.prototype.removeBookByTitle = function(title)
 {
   // Remove books from bookShelf matching title
 
-  var bDel = false;
+  var isDeleted = false;
   var posBook = this.checkBookByTitle(title);
 
   for (var i = 0;i < posBook.length;i++) {
     var nPos = this.checkBookByTitle(title);
     this.bookShelf.splice(nPos[0],1);
-    bDel = true;
+    isDeleted = true;
   }
 
-  return bDel; //true if book is removed - false if theres no match
+  return isDeleted; //true if book is removed - false if theres no match
 };
 
 Library.prototype.removeBookByAuthor = function(author)
 {
   // Remove books from bookShelf matching author's name
 
-  var bDel = false;
+  var isDeleted = false;
   var posBook = this.checkBookByAuthor(author);
 
   for (var i = 0;i < posBook.length;i++) {
     var nPos = this.checkBookByAuthor(author);
     this.bookShelf.splice(nPos[0],1);
-    bDel = true;
+    isDeleted = true;
   }
 
-  return bDel; //true if book is removed - false if theres no match
+  return isDeleted; //true if book is removed - false if theres no match
 };
 
 Library.prototype.getRandomBook = function()
@@ -101,66 +101,70 @@ Library.prototype.checkBookByAuthor = function(author)
 Library.prototype.getBookByTitle = function(title)
 {
   // Select books from bookShelf Completely or Partially matching title
-  var mBooks = [];
-  var lcTitle;
-  var lcBsTitle;
+  var arrBooks = [];
+  var lowercaseTitle;
+  var lowercaseBsTitle;
 
   for (var i = 0;i < this.bookShelf.length;i++) {
 
-    lcTitle = title.toLowerCase();
-    lcBsTitle = this.bookShelf[i].title.toLowerCase();
+    lowercaseTitle = title.toLowerCase();
+    lowercaseBsTitle = this.bookShelf[i].title.toLowerCase();
 
-    if (lcBsTitle.includes(lcTitle)) {
-      mBooks.push(this.bookShelf[i]);
+    if (lowercaseBsTitle.includes(lowercaseTitle)) {
+      arrBooks.push(this.bookShelf[i]);
     }
   }
-  return mBooks; //Books array
+  return arrBooks; //Books array
 };
 
 Library.prototype.getBookByAuthor = function(author)
 {
   // Select books from bookShelf Completely or Partially matching Author name
-  var mBooks = [];
-  var lcAuthor;
-  var lcBsAuthor;
+  var arrBooks = [];
+  var lowercaseAuthor;
+  var lowercaseBsAuthor;
 
+  // Loop through bookShelf
   for (var i = 0;i < this.bookShelf.length;i++) {
 
-    lcAuthor = author.toLowerCase();
-    lcBsAuthor = this.bookShelf[i].author.toLowerCase();
+    //Convert to lowercase both the autohr parameter and the author stored in bookShelf
+    lowercaseAuthor = author.toLowerCase();
+    lowercaseBsAuthor = this.bookShelf[i].author.toLowerCase();
 
-    if (lcBsAuthor.includes(lcAuthor)) {
-      mBooks.push(this.bookShelf[i]);
+    //Verifies if then parameter is included in then stored title in bookShelf
+    if (lowercaseBsAuthor.includes(lowercaseAuthor)) {
+      //If matches then pushes into books array
+      arrBooks.push(this.bookShelf[i]);
     }
   }
-  return mBooks; //Books array
+  return arrBooks; //returns Books array
 };
 
 Library.prototype.getBookByNumPages = function(numPages)
 {
   // Select books from bookShelf Completely matching numPages
-  var mBooks = [];
+  var arrBooks = [];
 
   for (var i = 0;i < this.bookShelf.length;i++) {
 
     if (this.bookShelf[i].numPages === numPages) {
-      mBooks.push(this.bookShelf[i]);
+      arrBooks.push(this.bookShelf[i]);
     }
   }
-  return mBooks; //Books array
+  return arrBooks; //Books array
 };
 
 Library.prototype.getBookByPubDate = function(pubDate)
 {
   // Select books from bookShelf Completely matching numPages
-  var mBooks = [];
+  var arrBooks = [];
 
   for (var i = 0;i < this.bookShelf.length;i++) {
     if (this.bookShelf[i].pubDate.includes(pubDate)) {
-      mBooks.push(this.bookShelf[i]);
+      arrBooks.push(this.bookShelf[i]);
     }
   }
-  return mBooks; //Books array
+  return arrBooks; //Books array
 };
 
 Library.prototype.getAllBooks = function()
@@ -173,36 +177,36 @@ Library.prototype.getAllBooks = function()
 
 Library.prototype.searchByAnyProperty = function(oBook)
 {
-  var sTitle = [];
-  var sAuthor = [];
-  var sNumPages = [];
-  var sPubDate = [];
-  var sResult = [];
+  var foundTitle = [];
+  var foundAuthor = [];
+  var foundNumPages = [];
+  var foundPubDate = [];
+  var foundResult = [];
 
 
   if (oBook.hasOwnProperty('title')) {
-    sTitle = this.getBookByTitle(oBook.title);
+    foundTitle = this.getBookByTitle(oBook.title);
   }
 
   if (oBook.hasOwnProperty('author')) {
-    sAuthor = this.getBookByAuthor(oBook.author);
+    foundAuthor = this.getBookByAuthor(oBook.author);
   }
 
   if (oBook.hasOwnProperty('numPages')) {
-    sNumPages = this.getBookByNumPages(oBook.numPages);
+    foundNumPages = this.getBookByNumPages(oBook.numPages);
   }
 
   if (oBook.hasOwnProperty('pubDate')) {
-    sPubDate = this.getBookByPubDate(oBook.pubDate);
+    foundPubDate = this.getBookByPubDate(oBook.pubDate);
   }
 
-  sResult = sTitle.concat(sAuthor, sNumPages, sPubDate);
+  foundResult = foundTitle.concat(foundAuthor, foundNumPages, foundPubDate);
 
 
   var unique_array = []
-  for(let i = 0;i < sResult.length; i++){
-    if(unique_array.indexOf(sResult[i]) == -1){
-        unique_array.push(sResult[i])
+  for(let i = 0;i < foundResult.length; i++){
+    if(unique_array.indexOf(foundResult[i]) == -1){
+        unique_array.push(foundResult[i])
     }
   }
 
@@ -212,18 +216,18 @@ Library.prototype.searchByAnyProperty = function(oBook)
 Library.prototype.addBooks = function(books)
 {
   // Adds books from Books array into bookShelf
-  var nBooks = 0;
+  var numBooks = 0;
 
   for (var i = 0;i < books.length;i++) {
 
     if (this.addBook(books[i])) {
-     //this.bookShelf.push(books[i]);
-     nBooks = nBooks + 1;
+     //Increase the Books counter
+     numBooks = numBooks + 1;
     }
 
   }
 
-  return nBooks; //Number of Books added to bookShelf
+  return numBooks; //Number of Books added to bookShelf
 };
 
 Library.prototype.getAuthors = function()
@@ -231,20 +235,20 @@ Library.prototype.getAuthors = function()
   // Select distinct authorName from bookShelf
 
   var authors = [];
-  var sAdd = true;
+  var shouldAdd = true;
 
   for (var i = 0;i < this.bookShelf.length;i++) {
 
     if (authors.length === 0) {
       authors.push(this.bookShelf[i].author);
     } else {
-      sAdd = true;
+      shouldAdd = true;
       for (var j = 0;j < authors.length;j++) {
        if (this.bookShelf[i].author === authors[j]) {
-        sAdd = false;
+        shouldAdd = false;
        }
       }
-      if (sAdd) {
+      if (shouldAdd) {
         authors.push(this.bookShelf[i].author);
       }
     }
@@ -269,23 +273,31 @@ Library.prototype.storeBookShelf = function()
 {
   // Store bookShelf on Local Storage
 
-  //convert JSON animal into a string
+  //convert JSON bookShelf into a string
   var dehydratedBookShelf = JSON.stringify(this.bookShelf);
 
   //save it with local storage
   window.localStorage.setItem('bookShelf', dehydratedBookShelf);
 
-  return true; //random authorName string from bookShelf
+  return true;
 };
 
 Library.prototype.restoreBookShelf = function()
 {
   // Restore bookShelf from Local Storage
 
+  this.bookShelf = [];
+
   //get 'bookShelf' and rehydrate it  (convert it back JSON)
   var rehydratedBookShelf = JSON.parse(window.localStorage.getItem('bookShelf'));
 
-  return rehydratedBookShelf;
+  for (var i = 0;i < rehydratedBookShelf.length;i++) {
+
+    this.addBook(new Book(rehydratedBookShelf[i]));
+
+  }
+
+  return this.bookShelf;
 }
 
 
@@ -302,5 +314,6 @@ document.addEventListener("DOMContentLoaded", function(e){
   window.book7 = new Book("The DaVinci Code", "Dan Brown", 296, "jan 1");
   window.book8 = new Book("Harry Potter", "J. K. Rowling", 198, "dec 1");
   window.book9 = new Book("The Mark of Athena", "Rick Riordan", 342, "dec 1");
+  window.book10 = new Book("Harry Potter", "J. K. Rowling", 200, "dec 1");
 
 });
